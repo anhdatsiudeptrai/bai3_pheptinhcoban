261bce5cc4adeb5b6a0f1cd35ff1f9e36efaa904
MAY 06 <MAY 06@LAB2-10>
2024-11-04 14:37:19 +0700

    Add project files.

---
+++ bai3_pheptinhcoban.cpp
@@ -0,0 +1,9 @@
+#include <iosteam>
+using namespace std;
+int main() {
+	cout << "chuong trinh tinh cac phep tinh co ban\n";
+	double so1, so2, tong, hieu, tich, thuong
+		cout << "nhap so1:"; cin>> so1;
+		cout << "nhap so2:"; cin>> so2;
+		return 0;
+}
